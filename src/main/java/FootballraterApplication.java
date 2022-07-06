bfbdb957c21d6ec37cf3396fd7d6150cbbfa24c9
Jacob Smith <jacobsmith1344@gmail.com>
2022-07-06 16:30:39 +0100

    Update FootballraterApplication.java

--- src/main/java/FootballraterApplication.java
+++ src/main/java/FootballraterApplication.java
@@ -1,5 +1,3 @@
-package com.jacob.footballrater;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
