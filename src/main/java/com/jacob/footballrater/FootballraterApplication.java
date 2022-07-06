dbe53310693f7ac5ca4ac1d45fd177b852d748a7
Jacob Smith <jacobsmith1344@gmail.com>
2022-07-06 16:38:43 +0100

    update pom

--- src/main/java/com/jacob/footballrater/FootballraterApplication.java
+++ src/main/java/com/jacob/footballrater/FootballraterApplication.java
@@ -1,3 +1,5 @@
+package com.jacob.footballrater;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
